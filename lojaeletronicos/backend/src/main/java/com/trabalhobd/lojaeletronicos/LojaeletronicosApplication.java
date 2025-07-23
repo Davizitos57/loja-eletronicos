ce8b9318d2899b469a1a9f06606ea30f0282916c
Davi Abner <davi.abner57@gmail.com>
2025-07-23 20:18:20 -0300

    Config: Configurando banco de dados

---
+++ lojaeletronicos/backend/src/main/java/com/trabalhobd/lojaeletronicos/LojaeletronicosApplication.java
@@ -0,0 +1,13 @@
+package com.trabalhobd.lojaeletronicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaeletronicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaeletronicosApplication.class, args);
+	}
+
+}
