d644fbd1ca112c341b723ff12e43215a6a449a13
tulio <tulio89lopes@gmail.com>
2025-08-18 03:50:34 -0300

    feat: implementa integracao de imagens no modal

--- lojaeletronicos/frontend/react/src/components/produto/modal/ProdutoImagem.jsx
+++ lojaeletronicos/frontend/react/src/components/produto/modal/ProdutoImagem.jsx
@@ -1,13 +1,21 @@
 import { Box, Chip } from '@mui/material';
+import api from '../../../services/api';
 
 export default function ProdutoImagem({ produto }) {
+
+    const isUploadedImage = produto.imagem && produto.imagem.startsWith('/uploads/');
+    
+    const imageUrl = isUploadedImage
+        ? `${api.defaults.baseURL}${produto.imagem}` 
+        : produto.imagem || `https://picsum.photos/800/600?random=${produto.id}`; 
+
     return (
         <Box
             sx={{
                 width: '40%',
                 minWidth: '400px',
                 height: '100%',
-                backgroundImage: `url(${produto.imagem || `https://picsum.photos/800/600?random=${produto.id}`})`,
+                backgroundImage: `url(${imageUrl})`,
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
                 position: 'relative',
